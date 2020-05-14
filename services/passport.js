dfa1ec331af53ffd458df770650be0c5b7c797fe
Dylan Lin <46723393+DylanLin29@users.noreply.github.com>
2020-05-13 19:43:36 -0700

    update proxy

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
             // query to check if the user is already exists
